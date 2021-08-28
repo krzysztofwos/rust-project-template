157a5eb0e2689f886c042b0b0214918891bdc340
Krzysztof Woś <krzysztof.wos@pm.me>
2021-08-28 21:58:59 +0900

    Rust

--- flake.nix
+++ flake.nix
@@ -1,10 +1,36 @@
 {
-  inputs.flake-compat = {
-    url = "github:edolstra/flake-compat";
-    flake = false;
+  inputs = {
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk = {
+      url = "github:nmattia/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
   };
-  outputs = { self, nixpkgs, ... }: {
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
-  };
+  outputs = { self, nixpkgs, flake-utils, naersk, ... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        naersk-lib = naersk.lib."${system}";
+        pkgs = nixpkgs.legacyPackages."${system}";
+        package-name = "hello-world";
+      in
+      {
+        devShell = nixpkgs.legacyPackages.${system}.mkShell {
+          nativeBuildInputs = with pkgs; [ cargo clippy rustc rustfmt ];
+        };
+        packages.${package-name} = naersk-lib.buildPackage {
+          pname = package-name;
+          root = ./.;
+        };
+        defaultPackage = self.packages.${system}.${package-name};
+        apps.${package-name} = flake-utils.lib.mkApp {
+          drv = self.packages.${system}.${package-name};
+        };
+        defaultApp = self.apps.${system}.${package-name};
+      }
+    );
 }
