5bbc64ca8de78cab944e0a5eee1b00b1550a929a
Krzysztof Woś <krzysztof.wos@pm.me>
2021-08-28 10:30:31 +0900

    Add flake-compat

--- flake.nix
+++ flake.nix
@@ -1,5 +1,9 @@
 {
-  outputs = { self, nixpkgs }: {
+  inputs.flake-compat = {
+    url = "github:edolstra/flake-compat";
+    flake = false;
+  };
+  outputs = { self, nixpkgs, ... }: {
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
     defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
   };
